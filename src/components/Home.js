499a77e58341b573dcad59a102ec875b53435017
Neelam <pariharneelam376@gmail.com>
2023-10-12 13:11:27 +0530

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -37,7 +37,7 @@ const About = () => {
         <button
           type="button"
           onClick={onButtonClick}
-          class="text-black bg-amber-500 m-10 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-black dark:text-black dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="text-black bg-amber-500 m-10 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-amber-500 dark:text-black dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Download CV
         </button>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from "react";
 import logo from "../assets/logo.png";
 const Navbar = () => {
   return (
-    <nav class="bg-amber-400 border-gray-200 dark:bg-orange-400 dark:text-black">
+    <nav class="bg-amber-400 border-gray-200 dark:bg-amber-400 dark:text-black">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a
           href="https://neelamnewportfolio.netlify.app/"
           class="flex items-center"
         >
           <img src={logo} class="h-8 mr-3" alt="Flowbite Logo" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span class="self-center text-2xl font-semibold whitespace-nowrap ">
             Neelam
           </span>
         </a>
@@ -18,7 +18,7 @@ const Navbar = () => {
             <button
               type="button"
               onclick="window.open('../assets/neelamresume.pdf')"
-              class="text-white bg-black hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-black dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:text-black"
+              class="text-white bg-black hover:bg-amber-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-black dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:text-white"
             >
               Contact Me
             </button>
@@ -52,11 +52,11 @@ const Navbar = () => {
           class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-cta"
         >
-          <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg   md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-amber-400 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg   md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-amber-400 ">
             <li>
               <a
                 href="/"
-                class="block py-2 pl-3 pr-4 text-white  rounded md:bg-transparent md:text-gray-900 md:p-0 md:dark:text-blue-500"
+                class="block py-2 pl-3 pr-4   rounded md:bg-transparent md:text-gray-900 md:p-0 md:dark:text-blue-500"
                 aria-current="page"
               >
                 Home
@@ -66,7 +66,7 @@ const Navbar = () => {
               <a
                 href="#about"
                 onclick="toggleMenu()"
-                class="block py-2 pl-3 pr-4 text-gray-900 rounded bg-amber-400 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded bg-amber-400 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
               >
                 About
               </a>
@@ -75,7 +75,7 @@ const Navbar = () => {
               <a
                 href="#skills"
                 onclick="toggleMenu()"
-                class="block py-2 pl-3 pr-4 text-gray-900 rounded bg-amber-400 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded bg-amber-400 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
               >
                 Skills
               </a>
@@ -84,7 +84,7 @@ const Navbar = () => {
               <a
                 href="#project"
                 onclick="toggleMenu()"
-                class="block py-2 pl-3 pr-4 text-gray-900 rounded bg-amber-400 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded bg-amber-400 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
               >
                 Project
               </a>
